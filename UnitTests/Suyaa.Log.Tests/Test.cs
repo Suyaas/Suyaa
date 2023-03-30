61e376148b1007fc4b2363f57e8c49ccc32eebe8
Lin Zheng <inmount@live.cn>
2023-03-30 18:32:23 +0800

    修改：【Log】 添加日志测试单元

---
+++ UnitTests/Suyaa.Log.Tests/Test.cs
@@ -0,0 +1,26 @@
+using Suyaa.Log.Loggers;
+using System.Diagnostics;
+using Xunit.Abstractions;
+
+namespace Suyaa.Log.Tests
+{
+    public class Test
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Test(ITestOutputHelper testOutput)
+        {
+            _output = testOutput;
+        }
+
+        [Fact]
+        public void Log()
+        {
+            sy.Logger.GetCurrentLogger().Use((string mesage) =>
+            {
+                Debug.WriteLine(mesage);
+            }).Use(new FileLogger("D:\\test.log"));
+            sy.Logger.Info("≤‚ ‘");
+        }
+    }
+}
