f808295f6030791ab207beae935c70ae5c237477
Lin Zheng <inmount@live.cn>
2023-03-30 19:21:38 +0800

    新增：【EFCore】 添加EFCore功能

---
+++ Suyaa.EFCore/Dbsets/Repository.cs
@@ -0,0 +1,206 @@
+﻿using Suyaa.Data;
+using Suyaa.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Suyaa.EFCore.Dbsets
+{
+    /// <summary>
+    /// 数据仓库
+    /// </summary>
+    public class Repository<TClass, TId> : IRepository<TClass, TId> where TClass : class, IEntity<TId>
+    {
+        // 私有变量
+        private Updater<TClass, TId> _updater;
+
+        /// <summary>
+        /// DB上下文
+        /// </summary>
+        public DbContext? Context { get; }
+
+        /// <summary>
+        /// 实体集合
+        /// </summary>
+        public DbSet<TClass>? DbSet { get; }
+
+        /// <summary>
+        /// 对象实例化
+        /// </summary>
+        /// <param name="context"></param>
+        public Repository(DbContext context)
+        {
+            Context = context;
+            DbSet = context.Set<TClass>();
+            _updater = new Updater<TClass, TId>(new DatabaseConnection(context.GetDatabaseConnectionInfo()));
+        }
+
+        /// <summary>
+        /// 对象实例化
+        /// </summary>
+        /// <param name="connection"></param>
+        public Repository(DatabaseConnection connection)
+        {
+            Context = null;
+            DbSet = null;
+            _updater = new Updater<TClass, TId>(connection);
+        }
+
+        /// <summary>
+        /// 删除数
+        /// </summary>
+        /// <param name="id"></param>
+        public void Delete(TId id)
+        {
+            DeleteAsync(id).Wait();
+        }
+
+        /// <summary>
+        /// 删除数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task DeleteAsync(TId id)
+        {
+            if (DbSet is null) throw new DatabaseException($"'DbSet'尚未定义");
+            var entity = await DbSet.FindAsync(id);
+            DbSet.Remove(entity);
+        }
+
+        /// <summary>
+        /// 添加数据
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Insert(TClass entity)
+        {
+            InsertAsync(entity).Wait();
+        }
+
+        /// <summary>
+        /// 添加数据
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public async Task InsertAsync(TClass entity)
+        {
+            if (DbSet is null) throw new DatabaseException($"'DbSet'尚未定义");
+            await DbSet.AddAsync(entity);
+        }
+
+        /// <summary>
+        /// 添加数据列表
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void InsertList(IEnumerable<TClass> entity)
+        {
+            InsertListAsync(entity).Wait();
+        }
+
+        /// <summary>
+        /// 添加数据列表
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public async Task InsertListAsync(IEnumerable<TClass> entity)
+        {
+            if (DbSet is null) throw new DatabaseException($"'DbSet'尚未定义");
+            await DbSet.AddRangeAsync(entity);
+        }
+
+        /// <summary>
+        /// 获取查询器
+        /// </summary>
+        /// <returns></returns>
+        public IQueryable<TClass> Query()
+        {
+            if (DbSet is null) throw new DatabaseException($"'DbSet'尚未定义");
+            return DbSet;
+        }
+
+        /// <summary>
+        /// 获取更新器
+        /// </summary>
+        /// <returns></returns>
+        public Updater<TClass, TId> Update() => _updater.UseAll();
+
+        /// <summary>
+        /// 获取更新器
+        /// </summary>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        public Updater<TClass, TId> Update(Expression<Func<TClass, object?>> selector) => _updater.Use(selector);
+
+        /// <summary>
+        /// 更新数据
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="predicate"></param>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        public async Task UpdateAsync(TClass entity, Expression<Func<TClass, bool>> predicate, Expression<Func<TClass, object?>>? selector = null)
+        {
+            if (Context is null) throw new DatabaseException($"'Context'尚未定义");
+            var updater = new Updater<TClass, TId>(this.Context.GetDatabaseConnection());
+            if (selector is null)
+            {
+
+            }
+            else
+            {
+                updater.Use(selector);
+            }
+            updater.Set(entity, predicate);
+            await Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// 更新数据
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Update(TClass entity)
+        {
+            UpdateAsync(entity).Wait();
+        }
+
+        /// <summary>
+        /// 更新数据
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public async Task UpdateAsync(TClass entity)
+        {
+            if (Context is null) throw new DatabaseException($"'Context'尚未定义");
+            Context.Entry(entity).State = EntityState.Modified;
+            await Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// 获取单行数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public TClass Get(TId id)
+        {
+            return GetAsync(id).Result;
+        }
+
+        /// <summary>
+        /// 获取单行数据
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<TClass> GetAsync(TId id)
+        {
+            if (DbSet is null) throw new DatabaseException($"'DbSet'尚未定义");
+            return await DbSet.FindAsync(id);
+        }
+    }
+}
---
+++ Suyaa.EFCore/Extensions/DbContextExtensions.cs
@@ -0,0 +1,126 @@
+﻿using Suyaa.Data;
+using Suyaa.EFCore.Dbsets;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Suyaa.EFCore
+{
+    /// <summary>
+    /// Egg专用数据库上下文
+    /// </summary>
+    public static class DbContextExtensions
+    {
+        // 数据表对象名称
+        private const string TYPE_NAME_DBSET = "Microsoft.EntityFrameworkCore.DbSet`1";
+        private const string TYPE_NAME_ENTITY = "Suyaa.EFCore.Dbsets.Entity`1";
+
+        // 获取实例基类定义类型
+        private static Type? GetEntityType(Type tp)
+        {
+            string tpName = tp.Namespace + "." + tp.Name;
+            if (tpName == TYPE_NAME_ENTITY) return tp;
+            if (tp.BaseType is null) return null;
+            return GetEntityType(tp.BaseType);
+        }
+
+        /// <summary>
+        /// 创建数据仓库
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="entity"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        private static object CreateRepositoty(DbContext context, Type entity, Type key)
+        {
+            Type tp = typeof(Dbsets.Repository<,>);
+            //指定泛型的具体类型
+            Type tpNew = tp.MakeGenericType(new Type[] { entity, key });
+            //创建一个list返回
+            return Activator.CreateInstance(tpNew, new object[] { context });
+        }
+
+        /// <summary>
+        /// 生成数据仓库集合
+        /// </summary>
+        /// <param name="context"></param>
+        public static List<object> GetRepositories(this DbContext context)
+        {
+            List<object> list = new List<object>();
+            var pros = context.GetType().GetProperties();
+            foreach (var pro in pros)
+            {
+                var tp = pro.PropertyType;
+                string tpName = tp.Namespace + "." + tp.Name;
+                if (tpName != TYPE_NAME_DBSET) continue;
+                var tpEntity = tp.GenericTypeArguments[0];
+                var tpEntityType = GetEntityType(tpEntity);
+                if (tpEntityType is null) continue;
+                var tpEntityKeyType = tpEntityType.GenericTypeArguments[0];
+                var obj = CreateRepositoty(context, tpEntity, tpEntityKeyType);
+                list.Add(obj);
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// 创建
+        /// </summary>
+        /// <param name="context"></param>
+        public static bool EnsureCreated<T>(this DbContext context) where T : IDbCreater, new()
+        {
+            T creater = new T();
+            return creater.EnsureCreated(context).Result;
+        }
+
+        /// <summary>
+        /// 获取数据库类型
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static DatabaseTypes GetDbType(this DbContext context) => context.Database.ProviderName switch
+        {
+            var h when h == "Microsoft.EntityFrameworkCore.Sqlite" => DatabaseTypes.Sqlite,
+            _ => throw new Exception($"不支持的数据库提供商'{context.Database.ProviderName}'")
+        };
+
+        /// <summary>
+        /// 获取数据库连接信息
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static IDatabaseConnectionInfo GetDatabaseConnectionInfo(this DbContext context)
+        {
+            var type = context.GetDbType();
+            return type switch
+            {
+                var h when h == DatabaseTypes.PostgreSQL => (IDatabaseConnectionInfo)Activator.CreateInstance(Type.GetType("Suyaa.Data.PostgreSQL.NpgsqlConnectionInfo")),
+                _ => throw new Exception($"不支持的数据库提供商'{context.Database.ProviderName}'")
+            };
+        }
+
+        /// <summary>
+        /// 获取数据库连接
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public static DatabaseConnection GetDatabaseConnection(this DbContext context)
+        {
+            return new DatabaseConnection(context.GetDatabaseConnectionInfo());
+        }
+
+        /// <summary>
+        /// 获取Sql语句供应器
+        /// </summary>
+        /// <param name="context"></param>
+        public static IDatabaseProvider GetSqlProvider(this DbContext context)
+        {
+            IDatabaseConnectionInfo info = context.GetDatabaseConnectionInfo();
+            Type type = Type.GetType(info.ProviderName);
+            return (IDatabaseProvider)Activator.CreateInstance(type);
+        }
+    }
+}
